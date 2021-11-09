cd360a3c8d28a62bac73da1f22c8a1030094cee4
alex <Marua.fadhel22@gmail.com>
2021-11-09 15:49:43 +0100

    we did add h1 to page1

---
+++ GetElementInJS.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("txt").value="hello"
+document.getElementById("btn").value="Login"
+
+
+
+window.txt2.value="hello2"        /*هذه طريقة اخرى لجلب الايدي للعنصر والتحكم فيه */
